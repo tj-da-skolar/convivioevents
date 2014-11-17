767843c8af6eaaed90960e8872ca94465a2fb0e7
tj-da-skolar <tj_da_skolar@yahoo.com>
2014-11-17 12:09:33 +0800

    database test

--- SignUp.php
+++ SignUp.php
@@ -3,9 +3,9 @@
 
 	session_start();
 	
-	$_SESSION['host'] = "localhost";
-	$_SESSION['user'] = "root";
-	$_SESSION['pass'] = "";
+	$_SESSION['host'] = "tcp:sh6p718bkv.database.windows.net";
+	$_SESSION['user'] = "official_convivio@yahoo.com@sh6p718bkv";
+	$_SESSION['pass'] = "Celebrate18";
 	$_SESSION['db'] = "convivio";
 
 	$con = mysql_connect($_SESSION['host'], $_SESSION['user'], $_SESSION['pass']);
